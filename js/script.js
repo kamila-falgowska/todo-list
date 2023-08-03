764b45ece7204e957ae4e0ae4b56139ca25d1a4d
Kamila Falgowska <kamilafalgowska@gmail.com>
2023-08-03 15:17:55 +0200

    Refactored task list according to immutability principle

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
 {
-  const tasks = [];
+  let tasks = [];
+  let hideDoneTasks = false;
 
   const removeTask = (taskIndex) => {
-    tasks.splice(taskIndex, 1);
+    tasks = [
+      ...tasks.slice(0, taskIndex),
+      ...tasks.slice(taskIndex + 1),
+    ];
     render();
   };
 
   const toggleTaskDone = (taskIndex) => {
-    tasks[taskIndex].done = !tasks[taskIndex].done;
+    tasks = [
+      ...tasks.slice(0, taskIndex),
+      {
+        ...tasks[taskIndex],
+        done: !tasks[taskIndex].done,
+      },
+      ...tasks.slice(taskIndex + 1),
+    ];
+
     render();
   };
 
   const addNewTask = (newTaskContent) => {
-    tasks.push({ content: newTaskContent });
+    tasks = [...tasks, { content: newTaskContent }];
     render();
   };
 
@@ -32,26 +44,36 @@
         removeTask(index);
       });
     });
-  }
+  };
 
-  const render = () => {
+  const renderTasks = () => {
     let tasksListHTMLContent = "";
 
     for (const task of tasks) {
       tasksListHTMLContent += `
-            <li class="tasks__item js-task">
-                <button class="tasks__button tasks__button--toggleDone js-toggleDone">
-                  ${task.done ? "✔" : ""}
-                </button>
-                <span class="tasks__content${task.done ? " tasks__content--done" : ""}">${task.content}</span>
-                <button class="tasks__button tasks__button--remove js-remove">🗑</button>
-            </li>
-          `;
+          <li class="tasks__item js-task">
+              <button class="tasks__button tasks__button--toggleDone js-toggleDone">
+                ${task.done ? "✔" : ""}
+              </button>
+              <span class="tasks__content${task.done ? " tasks__content--done" : ""}">${task.content}</span>
+              <button class="tasks__button tasks__button--remove js-remove">🗑</button>
+          </li>
+        `;
     }
 
     document.querySelector(".js-tasks").innerHTML = tasksListHTMLContent;
+  };
+
+  const renderButtons = () => { };
+
+  const bindButtonsEvents = () => { };
+
+  const render = () => {
+    renderTasks();
+    renderButtons();
 
     bindEvents();
+    bindButtonsEvents();
   };
 
   const onFormSubmit = (event) => {
